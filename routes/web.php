d63eb87817646c3a2b6af2169bb5ee1196e6be62
Ahmad Nurul Laiq <ahmadlaiq97@gmail.com>
2024-05-09 11:28:19 +0700

    tes ci/cd

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/cicd', function () {
+    return "CI/CD Laravel with Github Actions and Bash Script";
+});
